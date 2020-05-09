6a7fba4c129ee471a44a4667c20076621f981cb0
aleewolfram_school <aleewolfram@gmail.com>
2020-05-08 19:11:03 -0700

    plant changing functions and wind added

--- index.js
+++ index.js
@@ -52,6 +52,11 @@ let farmzoid2Pos = [19,18];
 let farmzoid3Pos = [19,20];
 let farmzoid4Pos = [20,19];
 
+let farmzoid1Path = [];
+let farmzoid2Path = [];
+let farmzoid3Path = [];
+let farmzoid4Path = [];
+
 //initializing arrays to hold the locations for easy locating
 
 for(var i=0; i < 40; i++)
@@ -124,6 +129,26 @@ function draw_farm( rminor, rmajor)
                 {
                     fill('Chocolate');
                 }
+                else if(farm[i][j] == "seed1")
+                {
+                    fill('SandyBrown');
+                }
+                else if(farm[i][j] == "stalk2" || farm[i][j] == "stalk3" || farm[i][j] == "stalk4")
+                {
+                    fill('MediumSeaGreen');
+                }
+                else if(farm[i][j] == "bush5" || farm[i][j] == "bush6" || farm[i][j] == "bush7" || farm[i][j] == "bush8" || farm[i][j] == "bush9" || farm[i][j] == "bush10")
+                {
+                    fill('Green');
+                }
+                else if(farm[i][j] == "flower11" || farm[i][j] == "flower12" || farm[i][j] == "flower13")
+                {
+                    fill('Pink');
+                }
+                else if(farm[i][j] == "fruit14" || farm[i][j] == "fruit15" || farm[i][j] == "fruit16")
+                {
+                    fill('Red');
+                }
                 else if(farm[i][j] == "river")
                 {
                     fill('Blue');
@@ -134,7 +159,7 @@ function draw_farm( rminor, rmajor)
                 }
                 else if(farm[i][j] == "barn")
                 {
-                    fill('Red');
+                    fill('DarkRed');
                 }
                 else if(farm[i][j] == "farmzoid1")
                 {
@@ -175,6 +200,10 @@ function setup() // P5 Setup Fcn
     let width = size * g_canvas.wid;
     let height = size * g_canvas.hgt;
     g_cnv = createCanvas( width, height );
+    console.log("Farmzoid1 = " + farmzoid1Pos);
+    console.log("Farmzoid2 = " + farmzoid2Pos);
+    console.log("Farmzoid3 = " + farmzoid3Pos);
+    console.log("Farmzoid4 = " + farmzoid4Pos);
     draw_farm( 20, 50);
 
 }
@@ -237,6 +266,11 @@ function printGoTo(farmzoidNum)
         s += "[" + arr[i][0] + ", " + arr[i][1] + "] ";
     }
     console.log(s);
+
+    let passVar = [];
+    passVar = farmzoid1_goto.shift();
+    console.log("PassedVAr= " + passVar);
+    find_path(1,farmzoid1Pos,passVar);
 }
 
 function incrementDate()
@@ -251,7 +285,7 @@ function whatToDo(weather)
     console.log("The weather passed in is: " + weather);
     if(weather == "Sunny")
     {
-       
+
 
     }
 
@@ -279,18 +313,22 @@ function showFarm()
 }
 
 //-------------------------------------------------DRAW LOGIC
-/*
+
 
 function draw()  // P5 Frame Re-draw Fcn, Called for Every Frame.
 {
     ++g_frame_cnt;
     if (0 == g_frame_cnt % g_frame_mod)
     {
-        farm[farmzoid1Pos[0]][farmzoid1Pos[1]]= "open";
-        farmzoid1Pos[0] += 1;
-        farmzoid1Pos[1] += 1;
-        farm[farmzoid1Pos[0]][farmzoid1Pos[1]]= "farmzoid1";
         console.log( "g_frame_cnt = " + g_frame_cnt );
+
+        console.log(farmzoid1Pos);
+        farm[farmzoid1Pos[0]][farmzoid1Pos[1]] = "open";
+        farmzoid1Pos = farmzoid1Path.shift();
+        console.log(farmzoid1Pos);
+        farm[farmzoid1Pos[0]][farmzoid1Pos[1]] = "farmzoid1";
+
+
         draw_update();
     }
 }
@@ -300,5 +338,134 @@ function draw_update()  // Update our display.
     console.log( "Call draw_farm" );
     draw_farm(20,50)
 }
-*/
 
+function find_path(zoidNum, startPoint, endPoint)
+{
+  let current = [];
+  let tempPath= [];
+  current = startPoint;
+  console.log("start path at " + current);
+  console.log("end path at " + endPoint);
+  let lookAhead = [0,0];
+
+  while (!neighbors (current, endPoint))
+  {
+    let lookAhead = [0,0];
+
+    if (current[0] > endPoint[0])
+    {
+      lookAhead[0] = current[0] - 1;
+    }
+    else if (current[0] < endPoint[0])
+    {
+      lookAhead[0] = current[0] + 1;
+    }
+    if (current[1] > endPoint[1])
+    {
+      lookAhead[1] = current[1] - 1;
+    }
+    else if (current[1] < endPoint[1])
+    {
+      lookAhead[1] = current[1] + 1;
+    }
+
+    switch(zoidNum)
+    {
+      case 1: console.log("Next step: " + lookAhead);
+              farmzoid1Path.push(lookAhead);
+              current = lookAhead;
+              break;
+    }
+  }
+}
+
+function neighbors(startPoint, endPoint)
+{
+  if (abs(startPoint[0] - endPoint[0]) <= 1 && abs(startPoint[1] - endPoint[1]) <= 1)
+  {
+    return true;
+  }
+  else {
+    {
+      return false;
+    }
+  }
+}
+
+function empty (rowCord, colCord)
+{
+  if (farm[rowCord][colCord] == "open")
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+//call this each time plot is visited and pass in the plot
+function advanceStage(x,y)
+{
+    if(farm[x][y] == "empty plot")
+    {
+        farm[x][y] = "seed1"
+    }
+    else if (farm[x][y] == "seed1")
+    {
+        farm[x][y] = "stalk2"
+    }
+    else if (farm[x][y] == "stalk2")
+    {
+        farm[x][y] = "stalk3"
+    }
+    else if (farm[x][y] == "stalk3")
+    {
+        farm[x][y] = "stalk4"
+    }
+    else if (farm[x][y] == "stalk4")
+    {
+        farm[x][y] = "bush5"
+    }
+    else if (farm[x][y] == "bush5")
+    {
+        farm[x][y] = "bush6"
+    }
+    else if (farm[x][y] == "bush6")
+    {
+        farm[x][y] = "bush7"
+    }
+    else if (farm[x][y] == "bush7")
+    {
+        farm[x][y] = "bush8"
+    }
+    else if (farm[x][y] == "bush8")
+    {
+        farm[x][y] = "bush9"
+    }
+    else if (farm[x][y] == "bush9")
+    {
+        farm[x][y] = "bush10"
+    }
+    else if (farm[x][y] == "bush10")
+    {
+        farm[x][y] = "flower11"
+    }
+    else if (farm[x][y] == "flower11")
+    {
+        farm[x][y] = "flower12"
+    }
+    else if (farm[x][y] == "flower12")
+    {
+        farm[x][y] = "flower13"
+    }
+    else if (farm[x][y] == "flower13")
+    {
+        farm[x][y] = "fruit14"
+    }
+
+    showFarm();
+
+
+
+}
